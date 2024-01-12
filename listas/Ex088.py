b8da123ca80501c9bb137d1441fbb71742672536
Henriqueas <matosh38@gmail.com>
2024-01-12 16:28:40 -0300

    Ex088.py

---
+++ listas/Ex088.py
@@ -0,0 +1,28 @@
+from random import randint
+
+lista = []
+jogos = []
+cont =0
+
+qtd = int(input('>>>'))
+while qtd !=0:
+    while True:
+        num = randint(1,60)
+        if num not in lista:
+            lista.append(num)
+            cont +=1
+        if cont ==6:
+            cont = 0
+            break
+    
+    jogos.append(lista[:])
+    lista.clear()
+    qtd -=1
+print(f'+{"-"*37}+')
+print(f'|{"MEGA SENA":^70}|')    
+print(f'+{"-"*37}+')
+for i,v in enumerate(jogos):
+    print(f'|{i+1}º jogo{f"{v}":^30}|')
+print(f'+{"-"*37}+')
+       
+        
